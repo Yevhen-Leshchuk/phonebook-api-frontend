3b7d68570a7e96d5ddd4094401a0a0e58e59021d
Yevhen-Leshchuk <evg.leschuk@gmail.com>
2022-03-15 22:38:20 +0200

    add contacts to localStorage

--- src/App.js
+++ src/App.js
@@ -20,6 +20,21 @@ class App extends Component {
   nameInputId = nanoid();
   numberInputId = nanoid();
 
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   addContact = data => {
     const { name, number } = data;
 
